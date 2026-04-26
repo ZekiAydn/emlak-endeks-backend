a589964f1540383995ab2eec8c8eea93071f6844
Zeki Aydın <105093184+ZekiAydn@users.noreply.github.com>
2026-04-26 18:40:19 +0300

    Add comparable image enrichment and media handling

--- src/controllers/location.controller.js
+++ src/controllers/location.controller.js
@@ -1,13 +1,34 @@
-import { fetchCities, fetchDistricts, fetchNeighborhoods } from "../services/addressDirectory.js";
+import {
+    fetchCities as fetchAddressCities,
+    fetchDistricts as fetchAddressDistricts,
+    fetchNeighborhoods as fetchAddressNeighborhoods,
+} from "../services/addressDirectory.js";
+import {
+    fetchCities as fetchTkgmCities,
+    fetchDistricts as fetchTkgmDistricts,
+    fetchNeighborhoods as fetchTkgmNeighborhoods,
+} from "../services/tkgmParcel.js";
 
 export async function listCities(_req, res) {
-    res.json(await fetchCities());
+    res.json(await fetchAddressCities());
 }
 
 export async function listDistricts(req, res) {
-    res.json(await fetchDistricts(req.query.cityId, req.query.cityName));
+    res.json(await fetchAddressDistricts(req.query.cityId, req.query.cityName));
 }
 
 export async function listNeighborhoods(req, res) {
-    res.json(await fetchNeighborhoods(req.query.districtId, req.query.districtName, req.query.cityName));
+    res.json(await fetchAddressNeighborhoods(req.query.districtId, req.query.districtName, req.query.cityName));
+}
+
+export async function listTkgmCities(_req, res) {
+    res.json(await fetchTkgmCities());
+}
+
+export async function listTkgmDistricts(req, res) {
+    res.json(await fetchTkgmDistricts(req.query.cityId));
+}
+
+export async function listTkgmNeighborhoods(req, res) {
+    res.json(await fetchTkgmNeighborhoods(req.query.districtId));
 }
--- src/routes/location.routes.js
+++ src/routes/location.routes.js
@@ -7,5 +7,8 @@ const router = Router();
 router.get("/locations/cities", authRequired, c.listCities);
 router.get("/locations/districts", authRequired, c.listDistricts);
 router.get("/locations/neighborhoods", authRequired, c.listNeighborhoods);
+router.get("/locations/tkgm/cities", authRequired, c.listTkgmCities);
+router.get("/locations/tkgm/districts", authRequired, c.listTkgmDistricts);
+router.get("/locations/tkgm/neighborhoods", authRequired, c.listTkgmNeighborhoods);
 
 export default router;
